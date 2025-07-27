926be0cd6643afeb2427e451da6c6445b050cca8
abhirupjgec02 <abhirupjgec02@gmail.com>
2025-07-27 10:38:22 -0400

    github pages installation to view project in github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/course2_module4_final_proj_e_plantShopping",
   plugins: [react()],
 })
